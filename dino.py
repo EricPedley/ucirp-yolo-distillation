2c80c570e0461bd8ec24d378e4a15457f42274e1
Eric Pedley <ericpedley@gmail.com>
2024-05-30 11:36:52 -0700

    set up pipeline from frames to YOLO dataset

--- dino.py
+++ dino.py
@@ -1,51 +1,59 @@
-from transformers import GroundingDinoForObjectDetection, GroundingDinoProcessor
+from transformers import GroundingDinoForObjectDetection, GroundingDinoProcessor, GroundingDinoImageProcessor
+from tqdm import tqdm
 import torch
-import cv2 as cv
-import matplotlib.pyplot as plt
 from PIL import Image
+import numpy as np
+import os
+import yaml
+
+CURRENT_ABSPATH = os.path.dirname(os.path.realpath(__file__))
+out_path = "yolo_output"
+split_proportions = [0.8, 0.1, 0.1]
+split_names = ["train", "valid", "test"]
+os.makedirs(out_path, exist_ok=True)
+for split_name in split_names:
+    os.makedirs(f"{out_path}/{split_name}", exist_ok=True)
+    os.makedirs(f"{out_path}/{split_name}/images", exist_ok=True)
+    os.makedirs(f"{out_path}/{split_name}/labels", exist_ok=True)
+
+metadata = {
+    "path": f"{CURRENT_ABSPATH}/{out_path}",
+    "train": f"{CURRENT_ABSPATH}/{out_path}/train/images",
+    "val": f"{CURRENT_ABSPATH}/{out_path}/valid/images",
+    "test": f"{CURRENT_ABSPATH}/{out_path}/test/images",
+    "nc": 1,
+    "names": ["rocket"]
+}
+yaml.dump(metadata, open(f"{out_path}/metadata.yaml", "w"))
 
 processor = GroundingDinoProcessor.from_pretrained("IDEA-Research/grounding-dino-base")
-model = GroundingDinoForObjectDetection.from_pretrained("IDEA-Research/grounding-dino-base")
-# Load the model
+model = GroundingDinoForObjectDetection.from_pretrained("IDEA-Research/grounding-dino-base").to('cuda')
+prompt_text = "rocket." # needs to be lowercase and end with a period
 
-text = "rocket"
-
-def preprocess_caption(caption: str) -> str:
-    result = caption.lower().strip()
-    if result.endswith("."):
-        return result
-    return result + "."
-
-
-# Predict a segment based on a point prompt
-image_path = "/home/ericp/autodistill/frames/Amateur rocket reaches 121,000 ft [sQw_C5KLhFM].webm_230.png"
-# image_path = "/home/ericp/autodistill/frames/Amateur rocket reaches 121,000 ft [sQw_C5KLhFM].webm_150.png"
-image = Image.open(image_path)
-inputs = processor(images=image, text=preprocess_caption(text), return_tensors="pt")
-print("predicting")
-with torch.no_grad():
-  outputs = model(**inputs)
-
-# colors for visualization
-COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
-          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
-
-def plot_results(pil_img, scores, labels, boxes):
-    plt.figure(figsize=(16,10))
-    plt.imshow(pil_img)
-    ax = plt.gca()
-    colors = COLORS * 100
-    for score, label, (xmin, ymin, xmax, ymax), c in zip(scores, labels, boxes, colors):
-        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                   fill=False, color=c, linewidth=3))
-        label = f'{text}: {score:0.2f}'
-        ax.text(xmin, ymin, label, fontsize=15,
-                bbox=dict(facecolor='yellow', alpha=0.5))
-    plt.axis('off')
-    plt.savefig("rocket_predicted.png")
-width, height = image.size
-postprocessed_outputs = processor.image_processor.post_process_object_detection(outputs,
+for frame_fname in tqdm(os.listdir("frames")):
+    frame_stem = frame_fname.split(".")[0]
+    image = Image.open(f"frames/{frame_fname}")
+    inputs = processor(images=image, text=prompt_text, return_tensors="pt")
+    for k,v in inputs.items():
+        if type(v) == torch.Tensor:
+            inputs[k] = v.to('cuda')
+    with torch.no_grad():
+        outputs = model(**inputs)
+    width, height = image.size
+    postprocessed_outputs = processor.image_processor.post_process_object_detection(outputs,
                                                                 target_sizes=[(height, width)],
                                                                 threshold=0.3)
-results = postprocessed_outputs[0]
-plot_results(image, results['scores'].tolist(), results['labels'].tolist(), results['boxes'].tolist())
+    results = postprocessed_outputs[0] 
+
+    scores, labels, boxes = results['scores'].tolist(), results['labels'].tolist(), results['boxes'].tolist()
+    if len(scores) == 0 and np.random.rand() < 0.9:
+        continue
+    spit_name = np.random.choice(split_names, p=split_proportions)
+    image.save(f"{out_path}/{split_name}/images/{frame_stem}.jpg")
+    with open(f"{out_path}/{split_name}/labels/{frame_stem}.txt", "w") as f:
+        for score, label, (xmin, ymin, xmax, ymax) in zip(scores, labels, boxes):
+            x_float = (xmin + xmax) / 2 / width
+            y_float = (ymin + ymax) / 2 / height
+            width_float = (xmax - xmin) / width
+            height_float = (ymax - ymin) / height
+            f.write(f"0 {x_float} {y_float} {width_float} {height_float}\n")
