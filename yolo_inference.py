207fc94468caadeca98184dd081a3700a8dba002
Eric Pedley <ericpedley@gmail.com>
2024-05-30 12:08:08 -0700

    add yolo inference and annotation test script

---
+++ yolo_inference.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import supervision as sv
+import cv2 as cv
+
+model = YOLO('runs/detect/train2/weights/best.pt')
+
+img = cv.imread("rocket.png")
+
+results = model(img)[0]
+detections = sv.Detections.from_ultralytics(results)
+
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+
+annotated_image = bounding_box_annotator.annotate(
+    scene=img, detections=detections)
+
+cv.imwrite("yolo_annotated_rocket.png", annotated_image)
